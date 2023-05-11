54d5211cf1948eea2d4d619c43ef7b0d3de51bff
Hemanya Sharma <hemanya.sharma@zoho.com>
2023-05-11 00:27:47 -0400

    Prep for more entities

--- Scripts/Game/Title.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    Console.writeline("Title screen ready.")
-
-func _on_Play_button_button_up():
-    get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
--- sign_apk.sh
+++ sign_apk.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/Android/Sdk/build-tools/33.0.2/apksigner sign --ks ~/.android/debug.keystore './2D_Test.apk'
+~/Android/Sdk/build-tools/33.0.2/apksigner sign --ks ~/.android/debug.keystore './Bruh.apk'
